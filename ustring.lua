164e094d8f46d6aced03cc7095c6e12df7da5fab
eccenux <eccenux+git@gmail.com>
2025-04-05 21:53:48 +0200

    basic toNFD

--- ustring.lua
+++ ustring.lua
@@ -8,4 +8,34 @@ local p = setmetatable({}, {
 	end
 })
 
+-- Very minimal Unicode Normalization
+local decompositionMap = {
+	[0x00C1] = {0x0041, 0x0301}, -- Á => A + ◌́
+	[0x00E9] = {0x0065, 0x0301}, -- é => e + ◌́
+}
+local function unicodeDecompose(cp)
+	return decompositionMap[cp]
+end
+function p.toNFD(s)
+	if type(s) ~= "string" then return nil end
+
+	local ok, u = pcall(utf8.codes, s)
+	if not ok then return nil end
+
+	local nfd = {}
+
+	for codepoint in utf8.codes(s) do
+		local decomposed = unicodeDecompose(codepoint)
+		if decomposed then
+			for _, cp in ipairs(decomposed) do
+				table.insert(nfd, utf8.char(cp))
+			end
+		else
+			table.insert(nfd, utf8.char(codepoint))
+		end
+	end
+
+	return table.concat(nfd)
+end
+
 return p
